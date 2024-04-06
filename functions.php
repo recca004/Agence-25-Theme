192b51982836f676855bf078a794eed1dc0c26d4
Mario N <14027939+recca004@users.noreply.github.com>
2024-04-07 00:51:08 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
 
 define( 'HELLO_ELEMENTOR_CHILD_VERSION', '3.0.0' );
@@ -22,13 +22,29 @@ define( 'HELLO_ELEMENTOR_CHILD_VERSION', '3.0.0' );
  * @return void
  */
 
-
-
 /* Function to enqueue stylesheet from parent theme */
-
 function child_enqueue__parent_scripts() {
-
     wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );
-
 }
 add_action( 'wp_enqueue_scripts', 'child_enqueue__parent_scripts' );
+
+// Ensure "Hello Elementor" theme is installed and activated
+function check_and_install_hello_elementor() {
+    if (!function_exists('wp_get_theme')) {
+        require_once(ABSPATH . 'wp-includes/theme.php');
+    }
+
+    $theme = wp_get_theme('hello-elementor');
+    if (!$theme->exists() || $theme->get('Status') != 'publish') {
+        require_once(ABSPATH . 'wp-admin/includes/theme.php');
+        wp_install_theme('hello-elementor');
+    }
+
+    // Check if "Hello Elementor" is the current theme, if not, activate it
+    $current_theme = wp_get_theme();
+    if ('hello-elementor' !== $current_theme->get_template()) {
+        switch_theme('hello-elementor');
+    }
+}
+add_action('after_switch_theme', 'check_and_install_hello_elementor');
+
